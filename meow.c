3b165c3031d4ddff5c970d1c8147927313ac0dbc
Petingo <eyanghongbin0315@gmail.com>
2018-12-21 23:54:33 +0800

    大家可以回家啦！！！！！

--- meow.c
+++ meow.c
@@ -7,35 +7,29 @@ struct iovec{
 };
 
 // functions:
-void read_config(char* filename, int* lastOffset, int* bookMarkCounter, int* bookMarks){
-  struct iovec iov[3];
+void read_config(char* filename, int* lastOffset, int* bookMarks){
+  struct iovec iov[2];
   iov[0].iov_base = lastOffset;
   iov[0].iov_len = 4;
 
-  iov[1].iov_base = bookMarkCounter;
-  iov[1].iov_len = 4;
-
-  iov[2].iov_base = bookMarks;
-  iov[2].iov_len = 400;
+  iov[1].iov_base = bookMarks;
+  iov[1].iov_len = 400;
 
   int fd = open(filename, O_RDONLY);
-  readv(fd, iov, 3);
+  readv(fd, iov, 2);
   close(fd);
 }
 
-void write_config(char* filename, int* lastOffset, int* bookMarkCounter, int* bookMarks){
-  struct iovec iov[3];
+void write_config(char* filename, int* lastOffset, int* bookMarks){
+  struct iovec iov[2];
   iov[0].iov_base = lastOffset;
   iov[0].iov_len = 4;
 
-  iov[1].iov_base = bookMarkCounter;
-  iov[1].iov_len = 4;
-
-  iov[2].iov_base = bookMarks;
-  iov[2].iov_len = 400;
+  iov[1].iov_base = bookMarks;
+  iov[1].iov_len = 400;
 
   int fd = open(filename, O_WRONLY);
-  writev(fd, iov, 3);
+  writev(fd, iov, 2);
   close(fd);
 }
 
@@ -58,6 +52,7 @@ static int booksFD[BOOK_AMOUNT];
 static int booksConfigFD[BOOK_AMOUNT];
 static char readBuffer[winWidth * winHeight];
 static char displayBuffer[winWidth * winHeight];
+static int pageStartOffset;
 static int offset;
 
 static char booksFileName[BOOK_AMOUNT][50] = {
@@ -69,12 +64,12 @@ static char booksFileName[BOOK_AMOUNT][50] = {
 };
 
 static char booksConfigFileName[BOOK_AMOUNT][50] = {
-  "HANSEL_AND_GRETEL_config.txt\0",
+  	"HANSEL_AND_GRETEL_config.txt\0",
 	"IRON_HANS_config.txt\0",
 	"RAPUNZEL_config.txt\0",
 	"THE_QUEEN_BEE_config.txt\0",
 	"THE_SEVEN_RAVENS_config.txt\0"
-}
+};
 
 static char booksName[BOOK_AMOUNT][50] = {
 	"Hansel and Gretel\0",
@@ -89,6 +84,8 @@ void processContent(){
   int contentHeight = 0;
   int i;
 
+  pageStartOffset = offset;
+  
   for(i = 0; i < winWidth * (winHeight - 1) - 1 && readBuffer[i] != '@' && contentHeight < winHeight - 1 ; i++){
     if(counter == winWidth || readBuffer[i] == '\n'){
       contentHeight++;
@@ -176,7 +173,6 @@ int main(int argc, char *argv[]){
   state = SELECT_BOOK;
   updateView();
   // varable use to save config
-  int lastOffset, bookMarkCounter;
   int bookMarks[100];
 
   while(state != EXIT){
@@ -214,11 +210,13 @@ int main(int argc, char *argv[]){
     }
     else if(state == START_READING){
       // Use config to set offset
-      read_config(booksConfigFileName[selectedBook], &lastOffset, 100, bookMarks);
-      offset = lastOffset;
-      state = READING;
+      read_config(booksConfigFileName[selectedBook], &offset, bookMarks);
+      //printf(1, "start_reading offset: %d\n", offset);
+      
       lseek(booksFD[selectedBook], offset, SEEK_SET);
       read(booksFD[selectedBook], readBuffer, winWidth * winHeight);
+
+      state = READING;
       processContent(displayBuffer);
       updateView();
     }
@@ -238,25 +236,32 @@ int main(int argc, char *argv[]){
           break;
 
         case 'j':
-          // 
+          {
           int mark_number;
           mark_number = (c[2] - '0') * 10 + (c[3] - '0');
-          lseek(booksFD[selectedBook], bookMarks[mark_number], SEEK_SET);
+          offset = bookMarks[mark_number];
+          lseek(booksFD[selectedBook], offset, SEEK_SET);
+          read(booksFD[selectedBook], readBuffer, winWidth * winHeight);
+          processContent(displayBuffer);
+
           updateView();
+          }
           break;
 
         case 'b':
+          {
           // book mark
           int mark_number;
           mark_number = (c[2] - '0') * 10 + (c[3] - '0');
-          bookMarks[mark_number] = offset;
-          updateView();
+          bookMarks[mark_number] = pageStartOffset;
+          }
           break;
 
         case 'q':
           state = SELECT_BOOK;
-          //
-          write_config(booksConfigFileName[selectedBook], &lastOffset, 100, bookMarks);
+          // printf(1, "q offset: %d, pageStart: %d\n", offset, pageStartOffset);
+          offset = pageStartOffset;
+          write_config(booksConfigFileName[selectedBook], &pageStartOffset, bookMarks);
           updateView();
           break;
       }  
@@ -266,6 +271,11 @@ int main(int argc, char *argv[]){
     }
   }
   printf(1, "Good Bye!\n");
+
+  for(int i = 0 ; i < BOOK_AMOUNT ; i++){
+    close(booksFD[i]);
+    close(booksConfigFD[i]);
+  }
   exit();
 }
 
